85ff4f2cda37c4b8f78c5f5ed74cd30a224e13aa
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-11-14 17:28:37 -0800

    Add unit tests for when a BTLeafNode is first constructed.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SRC = main.cc SqlParser.tab.c lex.sql.c SqlEngine.cc BTreeIndex.cc BTreeNode.cc RecordFile.cc PageFile.cc 
-TESTSRC = node-test.cc
+TESTSRC = node-test.cc SqlParser.tab.c lex.sql.c SqlEngine.cc BTreeIndex.cc BTreeNode.cc RecordFile.cc PageFile.cc 
 HDR = Bruinbase.h PageFile.h SqlEngine.h BTreeIndex.h BTreeNode.h RecordFile.h SqlParser.tab.h
 
 bruinbase: $(SRC) $(HDR)
@@ -12,11 +12,11 @@ SqlParser.tab.c: SqlParser.y
 	bison -d -psql $<
 
 # TODO: Comment out before submission, just in case TA uses this Makefile
-test: bruinbase $(TESTSRC)
+test: bruinbase $(TESTSRC) $(SRC)
 	bash queries.sh
-	g++ -o node-test $(TESTSRC)
+	g++ -o node-test $(TESTSRC) 
 	chmod 0755 node-test
 	./node-test
 
 clean:
-	rm -f bruinbase bruinbase.exe *.o *~ lex.sql.c SqlParser.tab.c SqlParser.tab.h newmovie.tbl indexmovie.tbl
+	rm -f bruinbase bruinbase.exe node-test *.o *~ lex.sql.c SqlParser.tab.c SqlParser.tab.h newmovie.tbl indexmovie.tbl
--- node-test.cc
+++ node-test.cc
@@ -1,11 +1,47 @@
 #include <cassert> 
 #include <cstdio>
+#include <cstring>
 
 #include "Bruinbase.h"
 #include "RecordFile.h"
+#include "PageFile.h"
 #include "BTreeNode.h"
 
 int main() 
 {
+    // Create PageFile that will store a leaf node
+    PageFile pf("node-test.txt", 'w');
+    assert(pf.getPageReadCount() == 0);
+    assert(pf.getPageWriteCount() == 0);
+    assert(pf.endPid() == 0); 
+
+    // The leaf node itself, which has just zeroed-out
+    // internal buffer initially. 
+    BTLeafNode leafNode;
+
+    // Key count is initially zero, as is next sibling PageId
+    assert(leafNode.getKeyCount() == 0); 
+    assert((int)leafNode.getNextNodePtr() == 0); 
+
+    // Debugging only
+    // printf("End PageId: %d\n", pf.endPid());
+    // printf("Return code: %d\n", leafNode.read(0, pf));
+
+    // Now we load the actual disk page, which requires
+    // us to write to the PageFile first, to set-up a page
+    char tempBuffer[1024];
+    memset(tempBuffer, 0, 1024);
+    pf.write(0, tempBuffer);
+
+    // Read should now succeed
+    assert(leafNode.read(0, pf) == 0);
+
+    // These should not have changed, as we wrote in all zeros
+    assert(leafNode.getKeyCount() == 0); 
+    assert((int)leafNode.getNextNodePtr() == 0); 
+
+
+
+    pf.close();
     return 0;
 }
